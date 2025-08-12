cb555d25a26c29f4d22a6e46b39402b2e388979e
filip.gorczyca141@gmail.com <filip.gorczyca141@gmail.com>
2025-08-12 07:53:53 +0200

    feat: add swagger

--- src/main/java/com/shortener/configs/SecurityConfig.java
+++ src/main/java/com/shortener/configs/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.shortener.configs;
 
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.*;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -20,7 +21,15 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable) //TODO enable
                 .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/signup", "/signin").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/signup", "/signin").permitAll()
+                                .requestMatchers(HttpMethod.GET,
+                                        "/swagger-ui.html",
+                                        "/swagger-ui/**",
+                                        "/v3/api-docs/**",
+                                        "/v3/api-docs.yaml",
+                                        "/error",
+                                        "/error/**"
+                                ).permitAll()
                         .anyRequest().authenticated()
                 );
         return http.build();
